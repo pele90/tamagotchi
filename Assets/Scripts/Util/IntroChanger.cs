8bdf74735fde6be29494d0980bb6a54f8e6171e6
Goran Petkovic <goranpetkovicpele90@gmail.com>
2019-06-04 19:57:55 +0200

    - Unity version upgrade to 2019.1.5

---
+++ Assets/Scripts/Util/IntroChanger.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class IntroChanger : MonoBehaviour
+{
+    public Image introImage;
+
+    public float modifier;
+
+    public int sceneNumber;
+
+    float currentAlpha = 0;
+
+    private Color color;
+
+    bool decrease = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentAlpha > 1)
+        {
+            decrease = true;
+        }
+
+        if (decrease)
+        {
+            currentAlpha -= modifier * Time.deltaTime;
+        }
+        else
+        {
+            currentAlpha += modifier * Time.deltaTime;
+        }
+
+        color = introImage.color;
+        color.a = currentAlpha;
+        introImage.color = color;
+
+        if (decrease && currentAlpha <= 0)
+        {
+            SceneManager.LoadScene(sceneNumber);
+        }
+    }
+}
